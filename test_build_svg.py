a1d5948c173a8f919660b4e7811a647bf117e48a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-07-06 09:43:42 +0300

    Remake is pytest

--- test_build_svg.py
+++ test_build_svg.py
@@ -3,7 +3,6 @@
 # PYTHON_ARGCOMPLETE_OK
 import os
 import subprocess
-import unittest
 
 
 file_str = """\
@@ -13,15 +12,13 @@ file_str = """\
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" width="342" height="216" viewBox="0 0 114 72"><rect x="0px" y="0px" width="342" height="216" fill="lightgray" /><rect x="1" y="1" width="36" height="10" fill="#00CCDE" stroke="black" /><text x="19" y="8" text-anchor="middle" font-size="4">Messagebox Window </text><rect x="39" y="1" width="36" height="10" fill="lightgray" stroke="None" /><text x="57" y="8" text-anchor="middle" font-size="4" /><rect x="77" y="1" width="36" height="10" fill="lightgray" stroke="None" /><text x="95" y="8" text-anchor="middle" font-size="4" /><rect x="1" y="13" width="36" height="10" fill="lightgray" stroke="None" /><text x="19" y="20" text-anchor="middle" font-size="4">Frame  </text><rect x="39" y="13" width="36" height="10" fill="lightgray" stroke="None" /><text x="57" y="20" text-anchor="middle" font-size="4" /><rect x="77" y="13" width="36" height="10" fill="lightgray" stroke="None" /><text x="95" y="20" text-anchor="middle" font-size="4" /><rect x="1" y="25" width="36" height="10" fill="lightgray" stroke="None" /><text x="19" y="32" text-anchor="middle" font-size="4" /><rect x="39" y="25" width="36" height="10" fill="White" stroke="black" /><text x="57" y="32" text-anchor="middle" font-size="4">Message text</text><rect x="77" y="25" width="36" height="10" fill="lightgray" stroke="None" /><text x="95" y="32" text-anchor="middle" font-size="4" /><rect x="1" y="37" width="36" height="10" fill="lightgray" stroke="None" /><text x="19" y="44" text-anchor="middle" font-size="4" /><rect x="39" y="37" width="36" height="10" fill="lightgray" stroke="None" /><text x="57" y="44" text-anchor="middle" font-size="4" /><rect x="77" y="37" width="36" height="10" fill="lightgray" stroke="None" /><text x="95" y="44" text-anchor="middle" font-size="4" /><rect x="1" y="49" width="36" height="10" fill="goldenrod" stroke="black" /><text x="19" y="56" text-anchor="middle" font-size="4">OK Button</text><rect x="39" y="49" width="36" height="10" fill="lightgray" stroke="None" /><text x="57" y="56" text-anchor="middle" font-size="4" /><rect x="77" y="49" width="36" height="10" fill="#ff0505" stroke="black" /><text x="95" y="56" text-anchor="middle" font-size="4">Cancel Button</text><rect x="1" y="61" width="36" height="10" fill="lightgray" stroke="None" /><text x="19" y="68" text-anchor="middle" font-size="4" /><rect x="39" y="61" width="36" height="10" fill="lightgray" stroke="None" /><text x="57" y="68" text-anchor="middle" font-size="4" /><rect x="77" y="61" width="36" height="10" fill="lightgray" stroke="None" /><text x="95" y="68" text-anchor="middle" font-size="4" /></svg>"""
 
 
-class TestBuildSVG(unittest.TestCase):
-    def test_file(self):
-        self.maxDiff = None
-        os.remove("message.svg")
-        subprocess.run(["python", "build_svg.py", "message.blk"])
-        with open("message.svg") as f:
-            s = f.read()
-            self.assertEqual(s, file_str)
+def test_file():
+    # Run the script
+    os.remove("message.svg")
+    subprocess.run(["python", "build_svg.py", "message.blk"], check=True)
 
+    # Read and compare output
+    with open("message.svg") as f:
+        s = f.read()
 
-if __name__ == "__main__":
-    unittest.main()
+    assert s == file_str
